b440dfbcc83d3e771cf20ebacb8b6b6ca409eab7
sheldu45 <shervinledu@hotmail.com>
2015-03-05 11:38:26 +0100

    Update Flotte.java

--- Flotte.java
+++ Flotte.java
@@ -2,10 +2,24 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+ /**
+ * Wikipédia : "Une flotte est un grand groupe de navires, d'importance numérique très variable".
+ * <p>
+ * Dans notre programme : "Une flotte est une liste de <a href="Bateau.html">Bateaux</a> (par héritage) + une variable de type Bateau qui est le bateau actuellement selectionné (eventuellement aucun bateaux n'est seléctionné si cette variable est null)"
+ * <p>
+ * <img src="FlotteRusse.jpg" alt="Un exemple de Flotte" style="width:304px;height:228px">
+ */
 
 public class Flotte extends ArrayList<Bateau> {
 
 	Bateau bateauSelectionne ;
+	
+	public Flotte() {
+	}
+	
+	public boolean estEnMer() {
+		return nombreDeBateauxNonPlaces() == 0;
+	}
 
 	public int nombreDeBateauxNonPlaces(){
 		int i = 0;
@@ -16,14 +30,6 @@ public class Flotte extends ArrayList<Bateau> {
 		}
 		
 		return i;
-	}
-	
-	public Flotte() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public boolean estEnMer() {
-		return nombreDeBateauxNonPlaces() == 0;
 	}
 	
 	public JPort getJPort(){
