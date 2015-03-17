b61142089cc6ed3c4eda71859ed256843d46a7d3
sheldu45 <shervinledu@hotmail.com>
2015-03-17 12:56:11 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
@@ -18,7 +19,7 @@ public class Partie {
 
 	
 	
-	static int DimensionDesPlateaux  = 10 ;	
+	static int 		DimensionDesPlateaux  = 10 ;	
 		
 	private int 	typeDePartie 	; 	// '1' <=> vs ordinateur 	; '2' <=> joueur1 vs joueur2	//mais il serait aussi possible d'imaginer une classe abstraite Partie et deux sous classes		
 	
@@ -26,9 +27,9 @@ public class Partie {
 	
 	private int[]	typeDeFlotte	;	//tableau d'entiers : à la i-ème case est indiqué le nombre de bateaux de taille i par joueur
 
-	private boolean phaseDeLaPartie	;	//False durant la phase du placement et True durant la phase de jeu et False à nouveau durant la phase de partie terminée
+	private int     phaseDeLaPartie	;	//0 durant la phase du placement 1 durant la phase de jeu et 2 durant la phase de partie terminée
 	
-	private boolean tourDuJoueur	; 	//False au tour du Joueur1 et True au tour du Joueur2
+	private int		tourDuJoueur	; 	//1 au tour du Joueur1 et 2 au tour du Joueur2
 	
 	private Plateau plateauDuJoueur1	;	//plateau du joueur1
 	
@@ -42,13 +43,13 @@ public class Partie {
 	public Partie(int typeDePartie, int typeDeCases, int[] typeDeFlotte){
 
 		assert typeDeFlotte[0]  == 0 : 	"car il doit y avoir 0 bateaux de taille 0 ! typeDeFlotte est un tableau qui à chaque indice i indique le nombre de bateaux de taille i par joueur ";
-	        assert typeDeFlotte[1]  == 0 : 	"car il doit y avoir 0 bateaux de taille 1 ! ";
+	    assert typeDeFlotte[1]  == 0 : 	"car il doit y avoir 0 bateaux de taille 1 ! ";
 		
 		this.typeDePartie 		= typeDePartie;
 		this.typeDeCases 		= typeDeCases;
 		this.typeDeFlotte 		= typeDeFlotte;
-		this.phaseDeLaPartie	= false;
-		this.tourDuJoueur 		= false;
+		this.phaseDeLaPartie	= 0;
+		this.tourDuJoueur 		= 1;
 		
 		switch(typeDeCases){			// 4 pour des cases carrées	;	6 pour des cases hexagonales 
 			case 4 :
@@ -98,7 +99,7 @@ public class Partie {
 		/*(d)*/			fenetreDeJeu.setLocationRelativeTo(null);
 		/*(e)*/			fenetreDeJeu.setVisible(true);
 						fenetreDeJeu.setResizable(false);
-						
+						fenetreDeJeu.setLayout(new BorderLayout());
 					
 		
 		
@@ -107,55 +108,106 @@ public class Partie {
 		
 			case 2 :
 				
-				plateauDuJoueur1.visible = true ;
-				plateauDuJoueur1.setMinimumSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
-				plateauDuJoueur1.setPreferredSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
-				
-				
-				
-				JPort  port    = new JPort(plateauDuJoueur1.listeDesBateaux);
-				port.setMinimumSize(new Dimension(fenetreDeJeu.getWidth(), 150));
-				port.setPreferredSize(new Dimension(fenetreDeJeu.getWidth(), 150));
-				port.setBackground(Color.red);
-				
-				
-				
-				fenetreDeJeu.setLayout(new BorderLayout());
-				fenetreDeJeu.add(plateauDuJoueur1, BorderLayout.CENTER);
-				fenetreDeJeu.add(port, BorderLayout.SOUTH);
-				
-				//fenetreDeJeu.setContentPane(total);
-			    fenetreDeJeu.pack();
-			    fenetreDeJeu.repaint();
+				miseEnPlace(1);
+				miseEnPlace(2);
+				this.phaseDeLaPartie = 1;
 				
 				break;
-		}
-		
 				
-		
-		
-		//quand PhaseDeLaPartie sera true, on passera à la phase de jeu ; tant qu'elle est false, c'est la phase de positionnement
-		while(phaseDeLaPartie == false){
-			
+				
 		}
-		
-		//quand PhaseDeLaPartie sera false, on passera à la phase de partie terminée ; tant qu'elle est true, c'est la phase de jeu
-		while(phaseDeLaPartie == true){
-			
+						fenetreDeJeu.setBackground(Color.BLACK);
+						GridLayout gl = new GridLayout(1, 2);
+						gl.setHgap(10);
+						fenetreDeJeu.setLayout(gl);
+		//quand PhaseDeLaPartie sera 2, on passera à la phase de partie terminée ; tant qu'elle est 1, c'est la phase de jeu
+		while(phaseDeLaPartie == 1){
+			tour(1);
 		}
 		
 		//c'est la fin de la partie, voulez-vous rejouer?
-		while(phaseDeLaPartie == true){
+		while(phaseDeLaPartie == 2){
 			
 		}
 		
 	}
 
+	public void miseEnPlace(int joueur){
+		
+		if (joueur == 1){ 
+			plateauDuJoueur1.visible = true ;
+		    plateauDuJoueur1.setMinimumSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			plateauDuJoueur1.setPreferredSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			
+			JPort  port    = new JPort(plateauDuJoueur1.listeDesBateaux);
+			port.setMinimumSize(new Dimension(fenetreDeJeu.getWidth(), 150));
+			port.setPreferredSize(new Dimension(fenetreDeJeu.getWidth(), 150));
+			port.setBackground(Color.red);
+			
+			fenetreDeJeu.add(plateauDuJoueur1, BorderLayout.CENTER);
+			fenetreDeJeu.add(port, BorderLayout.SOUTH);
+	
+		    fenetreDeJeu.pack();
+		    fenetreDeJeu.repaint();
+		    
+		    while(plateauDuJoueur1.phaseDePositionnement){
+		    	System.out.println();
+			}
 
+			fenetreDeJeu.remove(plateauDuJoueur1);
+			fenetreDeJeu.remove(port);
+		}
+		
+		if (joueur == 2){ 
+			plateauDuJoueur2.visible = true ;
+		    plateauDuJoueur2.setMinimumSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			plateauDuJoueur2.setPreferredSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			
+			JPort  port2    = new JPort(plateauDuJoueur2.listeDesBateaux);
+			port2.setMinimumSize(new Dimension(fenetreDeJeu.getWidth(), 150));
+			port2.setPreferredSize(new Dimension(fenetreDeJeu.getWidth(), 150));
+			port2.setBackground(Color.red);
+			
+			fenetreDeJeu.add(plateauDuJoueur2, BorderLayout.CENTER);
+			fenetreDeJeu.add(port2, BorderLayout.SOUTH);
+	
+		    fenetreDeJeu.pack();
+		    fenetreDeJeu.repaint();
+		    
+		    while(plateauDuJoueur2.phaseDePositionnement){
+		    	System.out.println();	
+			}
+
+			fenetreDeJeu.remove(plateauDuJoueur2);
+			fenetreDeJeu.remove(port2);
+		}
+		
+	}
+	
+	
+	public void tour(int joueur){
+		
+		if (tourDuJoueur == 1){ 
+			plateauDuJoueur1.visible = true ;
+			plateauDuJoueur2.visible = false ;
+			
+			plateauDuJoueur1.setMinimumSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			plateauDuJoueur1.setPreferredSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			plateauDuJoueur2.setMinimumSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			plateauDuJoueur2.setPreferredSize(new Dimension(Partie.DimensionDesPlateaux*PlateauCarre.largeurCase, Partie.DimensionDesPlateaux*PlateauCarre.largeurCase));
+			
+			fenetreDeJeu.add(plateauDuJoueur1);
+			fenetreDeJeu.add(plateauDuJoueur2);
+			
+			fenetreDeJeu.pack();
+		    fenetreDeJeu.repaint();
+		}
+	}
+	
 
 
 	public static void main(String[] args) {
-		Partie p = new Partie(2, 4, new int[] {0, 0, 1, 1, 1, 1, 1});
+		Partie p = new Partie(2, 4, new int[] {0, 0, 2});
 	}
 
 }
